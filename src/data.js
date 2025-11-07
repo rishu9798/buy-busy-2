05430c3f4680d55a07486fcd8efb070458a1f2ad
rishu9798 <126560565+rishu9798@users.noreply.github.com>
2025-11-07 23:14:57 +0530

    Intial commit

---
+++ src/data.js
@@ -0,0 +1,208 @@
+export const data = [
+  {
+    price: 1099,
+    category: "men's clothing",
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    rating: "{count: 120, rate: 3.9}",
+    id: 1,
+    image: "https://tse2.mm.bing.net/th/id/OIP.xw7GDLjG2Zd1LLX1xRkx5AHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",
+  },
+  {
+    title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+    category: "electronics",
+    price: 699,
+    image: "https://purepng.com/public/uploads/large/purepng.com-sandisk-usb-flash-pen-driveelectronics-pen-drive-usb-9415246650490ar6r.png",
+    description:
+      "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+    rating: "{count: 470, rate: 2.9}",
+    id: 10,
+  },
+  {
+    rating: "{count: 319, rate: 4.8}",
+    image: "https://m.media-amazon.com/images/I/61xjcvGS6AL._AC_SX569_.jpg",
+    price: 5000,
+    category: "electronics",
+    description:
+      "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
+    id: 11,
+    title:
+      "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
+  },
+  {
+    image: "https://tse3.mm.bing.net/th/id/OIP.mvilMtObDA6DzP3y7cMvsgHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",
+    title:
+      "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+    price: 25000,
+    category: "electronics",
+    description:
+      "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
+    rating: "{count: 400, rate: 4.8}",
+    id: 12,
+  },
+  {
+    price: 19999,
+    title: "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+    category: "electronics",
+    description:
+      "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
+    image: "https://www.ryans.com/storage/products/main/acer-ek220q-e3bi-215-inch-full-hd-display-hdmi-21708430188.webp",
+    id: 13,
+    rating: "{count: 250, rate: 2.9}",
+  },
+  {
+    rating: "{count: 140, rate: 2.2}",
+    price: 70000,
+    image: "https://images.samsung.com/is/image/samsung/assets/consumer/global/vd/monitors/odyssey/odyssey-introducing-detail-neo-mo.jpg?$ORIGIN_JPG$",
+    title:
+      "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED ",
+    category: "electronics",
+    id: 14,
+    description:
+      "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag",
+  },
+  {
+    price: 2599,
+    image: "https://tse1.mm.bing.net/th/id/OIP.AqpsK9ZGyxVD-IxefT_pSgHaJ0?rs=1&pid=ImgDetMain&o=7&rm=3",
+    title: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+    category: "women's clothing",
+    description:
+      "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable H...",
+    rating: "{count: 235, rate: 2.6}",
+    id: 15,
+  },
+  {
+    rating: "{count: 340, rate: 2.9}",
+    image: "https://m.media-amazon.com/images/I/71maQ4MGfYL.jpg",
+    id: 16,
+    description:
+      "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+    category: "women's clothing",
+    price: 2599,
+    title:
+      "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+  },
+  {
+    image:"https://m.media-amazon.com/images/I/61IyMC9ZxzS._AC_UL1500_.jpg",
+    id: 17,
+    price: 3333,
+    title: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+    category: "women's clothing",
+    rating: "{count: 679, rate: 3.8}",
+    description:
+      "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
+  },
+  {
+    description:
+      "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
+    image: "https://thumbs.dreamstime.com/z/palestinian-women-clothing-beautiful-traditional-37991821.jpg",
+    category: "women 's clothing",
+    title: "MBJ Women's Solid Short Sleeve Boat Neck V ",
+    price: 799,
+    id: 18,
+    rating: "{count: 130, rate: 4.7}",
+  },
+  {
+    price: 699,
+    id: 19,
+    description:
+      "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
+    title: "Opna Women's Short Sleeve Moisture",
+    category: "women's clothing",
+    rating: "{count: 146, rate: 4.5}",
+    image: "https://m.media-amazon.com/images/I/91NYdxLmOML._AC_CR0%2C0%2C0%2C0_SX352_SY330_.jpg",
+  },
+  {
+    id: 2,
+    category: "men's clothing",
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    title: "Mens Casual Premium Slim Fit T-Shirts ",
+    image:
+      "https://www.thefashionisto.com/wp-content/uploads/2017/07/Cristiano-Ronaldo-2017-CR7-Denim-Collection-Launch-003-450x0.jpg",
+    price: 300,
+    rating: "{count: 259, rate: 4.1}",
+  },
+  {
+    price: 999,
+    id: 20,
+    title: "DANVOUY women's T Shirt Casual Cotton trouser",
+    description:
+      "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+    category: "women's clothing",
+    rating: "{count: 145, rate: 3.6}",
+    image: "https://content.woolovers.com/img/747x856/7ab95-77136_w23l_navy_w_11.jpg",
+  },
+  {
+    price: 1155,
+    id: 3,
+    category: "men's clothing",
+    description:
+      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    rating: "{count: 500, rate: 4.7}",
+    title: "Mens Cotton Jacket",
+    image: "https://i.pinimg.com/originals/bd/5d/81/bd5d812d92b55695b3691a09b2c681ce.png",
+  },
+  {
+    rating: "{count: 430, rate: 2.1}",
+    price: 900,
+    category: "men's clothing",
+    image: "https://tse4.mm.bing.net/th/id/OIP.fctbCtfZW3H-tpGVYd1dcAHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
+    id: 4,
+    title: "Mens Casual Slim Fit",
+    description:
+      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+  },
+  {
+    price: 6950,
+    description:
+      "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+    image: "https://www.topofstyle.com/image/1/00/7r/ta/1007rta-john-hardy-women-s-legends-naga-gold-silver-dragon-station_500x660_0.jpg",
+    rating: "{count: 400, rate: 4.6}",
+    category: "jewelery",
+    id: 5,
+    title:
+      "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+  },
+  {
+    price: 7500,
+    image: "https://m.media-amazon.com/images/I/615ZzOEVEaL._AC_SY625_.jpg",
+    category: "jewelery",
+    title: "Solid Gold Petite Micropave ",
+    id: 6,
+    rating: "{count: 70, rate: 3.9}",
+    description:
+      "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+  },
+  {
+    id: 7,
+    image: "https://i5.walmartimages.com/seo/Lyssa-Jewel-14K-White-Gold-Over-Princess-Cut-1-9-Ct-3-Stone-Engagement-Ring-Lab-Created-Simulated-Diamond-Trilogy-Her-Prong-Set-Wedding-Ideal-Gift-We_395f57b4-1c4a-4e73-b810-55b1823d24e9.b0c82ddb3711fb1f66be17f7697c88b1.jpeg",
+    category: "jewelery",
+    title: "White Gold Plated Princess",
+    description:
+      "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
+    rating: "{count: 400, rate: 3}",
+    price: 3999,
+  },
+  {
+    description:
+      "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+    title: "Pierced Owl Rose Gold Plated Stainless Steel Double",
+    price: 10999,
+    image: "https://tse3.mm.bing.net/th/id/OIP.SH124bAmHT_XXVN_b_QmtgHaDq?rs=1&pid=ImgDetMain&o=7&rm=3",
+    id: 8,
+    rating: "{count: 100, rate: 1.9}",
+    category: "jewelery",
+  },
+  {
+    rating: "{count: 203, rate: 3.3}",
+    description:
+      "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
+    image: "https://asset.conrad.com/media10/isa/160267/c1/-/en/1575062_LB_02_FB/image.jpg",
+    price: 999,
+    category: "electronics",
+    title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+    id: 9,
+  },
+];
